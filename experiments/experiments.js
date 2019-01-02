ba14f4f732d10c61b9728bf5975f796ee0cca399
Laura Sanchez <lsanchezgmz@gmail.com>
2019-01-02 14:31:15 +0100

    addind booleans and control flow

--- experiments/experiments.js
+++ experiments/experiments.js
@@ -2,3 +2,22 @@ const monthlyRent = 500;
 
 const yearlyRent = monthlyRent * 12;
 console.log(yearlyRent);
+
+const firstName = "Laura";
+const lastName = "Sanchez";
+
+const sentence = "Hello " + firstName + " " +  lastName + "! How are you!?";
+const sentenceWithTemplate = `Hello ${firstName} ${lastName}! How are you!?`;
+
+console.log(sentence);
+console.log(sentenceWithTemplate);
+
+//comments in JS are introduced like this. You can also use /* as in CSS.
+
+const skyIsBlue = true;
+
+if (skyIsBlue) {
+    console.log("The sky is blue!");
+} else {
+    console.log("The sky is ... not blue?");
+}
