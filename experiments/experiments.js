fd9a8fd2ed85e3d98a9bc669b5c03d900b07eb7c
Laura Sanchez <lsanchezgmz@gmail.com>
2019-01-03 12:55:19 +0100

    loops

--- experiments/experiments.js
+++ experiments/experiments.js
@@ -14,6 +14,7 @@ console.log(sentenceWithTemplate);
 
 //comments in JS are introduced like this. You can also use /* as in CSS.
 
+//Control Flow
 const skyIsBlue = true;
 
 if (skyIsBlue) {
@@ -21,3 +22,81 @@ if (skyIsBlue) {
 } else {
     console.log("The sky is ... not blue?");
 }
+
+const friendsAtYourParty = 10;
+
+if (friendsAtYourParty === 0) {
+  console.log("Cool, now I have a lot of nachos to myself.");
+} else if (friendsAtYourParty >= 4) {
+  console.log("Perfect amount to play some Mario Kart.");
+} else {
+  console.log("Wooooo turn on the dance music!");
+}
+
+//loop
+let friendsAtYourParty = 0;
+friendsAtYourParty = friendsAtYourParty + 1;
+friendsAtYourParty = friendsAtYourParty + 1;
+friendsAtYourParty = friendsAtYourParty + 1;
+friendsAtYourParty = friendsAtYourParty + 1;
+friendsAtYourParty = friendsAtYourParty + 1;
+friendsAtYourParty = friendsAtYourParty + 1;
+friendsAtYourParty = friendsAtYourParty + 1;
+friendsAtYourParty = friendsAtYourParty + 1;
+friendsAtYourParty = friendsAtYourParty + 1;
+friendsAtYourParty = friendsAtYourParty + 1;
+console.log(friendsAtYourParty);
+
+
+//while loop
+let friendsAtYourParty = 0;
+while (friendsAtYourParty < 10) {
+    friendsAtYourParty = friendsAtYourParty + 1;
+}
+console.log(friendsAtYourParty);
+
+//All lines below do the same, in this case, they add 1 to something
+let friendsAtYourParty = 0;
+friendsAtYourParty = friendsAtYourParty + 1;
+friendsAtYourParty += 1;
+friendsAtYourParty++;
+++friendsAtYourParty;
+console.log(friendsAtYourParty);
+
+//For loop, with 3 parts and starts with a let: a control variable (i), a condition and the intruction on what to do at the end of the loop.
+let friendsAtYourParty = 0;
+for (let i = 0; i <= 10; i++) {
+  friendsAtYourParty++;
+}
+console.log(friendsAtYourParty);
+
+
+//Functions
+
+function addTwo(number) {
+    return number + 2;
+  }
+  
+  const finalAnswer = addTwo(5);
+  console.log(finalAnswer);
+
+  function greet(firstName, lastName, honorific, greeting) {
+    return `${greeting} ${honorific} ${lastName}! I’m extremely pleased you could join us, ${firstName}! I hope you enjoy your stay, ${honorific} ${lastName}.`;
+  }
+  
+  console.log(greet("Brian", "Holt", "Lord", "Salutations"));
+  console.log(greet("Jack", "Sparrow", "Captain", "A-hoy"));
+
+
+const myHomeCity = "Salt Lake City";
+const myHomeState = "Utah";
+const myHomeCountry = "USA";
+
+function logOutYourHome(city, state, country) {
+  console.log(`You are from ${city}, ${state} ${country}.`);
+}
+
+logOutYourHome(myHomeCity, myHomeState, myHomeCountry);
+logOutYourHome("Torino", "Piemonte", "Italia");
+
+//Scope
